aaf1dd191defb31630fd7cd8d62e7f68c8e632ce
Sean <seanreilly123@hotmail.com>
2024-04-22 18:55:36 +1000

    Antd added, react query search runs better

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -5,25 +5,35 @@ import { useDebounce } from "@uidotdev/usehooks";
 
 type Props = {};
 
+type SearchedStock = {
+    "1. symbol": string;
+    "2. name": string;
+    "3. type": string;
+    "4. region": string;
+    "5. marketOpen": string;
+    "6. marketClose": string;
+    "7. timezone": string;
+    "8. currency": string;
+    "9. matchScore": string;
+};
+
 const SearchBar = (props: Props) => {
     const [search, setSearch] = useState("");
     const debouncedSearch = useDebounce(search, 500);
 
-    const searchStocks = async () => {
-        if (!search && !debouncedSearch) return [];
+    const searchStocks = async (keyword: string) => {
         const res = await fetch(
-            `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${debouncedSearch}&apikey=891N0XBQAZW5FS4Q`
+            `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keyword}&apikey=891N0XBQAZW5FS4Q`
         );
         return res.json();
     };
 
     const { data, isLoading } = useQuery({
-        queryKey: ["search", search],
-        queryFn: searchStocks,
+        queryKey: ["search", debouncedSearch],
+        queryFn: () => searchStocks(debouncedSearch),
+        enabled: !!debouncedSearch,
     });
-
     console.log(data);
-
     return (
         <div>
             <input
@@ -32,6 +42,11 @@ const SearchBar = (props: Props) => {
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
             />
+            <div>
+                {data?.bestMatches.map((stock: SearchedStock, i: number) => (
+                    <p key={i}>{stock["1. symbol"]}</p>
+                ))}
+            </div>
         </div>
     );
 };
--- app/components/StockList.tsx
+++ app/components/StockList.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import styles from "../page.module.css";
+import { Skeleton } from "antd";
 
 type Props = {};
 
 const StockList = (props: Props) => {
     return (
         <div className={styles["stock-list-grid"]}>
-            {[1, 2, 3, 4, 5, 6].map((t, i) => (
-                <div key={i}>
-                    <h3>Stock {i}</h3>
-                </div>
+            {[1, 2, 3, 4, 5, 6].map((t) => (
+                <Skeleton key={t} />
             ))}
         </div>
     );
