3d386409097f01d440af792228eba30c2b43b92e
Sean <seanreilly123@hotmail.com>
2024-05-17 18:52:48 +1000

    Analytics removed temp

--- app/server/firebase.js
+++ app/server/firebase.js
@@ -27,8 +27,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, analytics, auth, db };
+export { app, auth, db };
