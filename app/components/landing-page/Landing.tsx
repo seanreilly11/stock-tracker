fa09cb4198df9bf1e90c76208b6a3e06fc0690e3
Sean <seanreilly123@hotmail.com>
2024-08-07 16:12:24 +1000

    Fullscreen loader returned to landing page for auth split second

--- app/components/landing-page/Landing.tsx
+++ app/components/landing-page/Landing.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "./Banner";
 import Features from "./Features";
 import Stats from "./Stats";
+import LoaderFullscreen from "../common/LoaderFullscreen";
+import useAuth from "@/hooks/useAuth";
 
 const Landing = () => {
+    const [showLoader, setShowLoader] = useState(true);
+    const { user } = useAuth();
+    useEffect(() => {
+        user && setShowLoader(false);
+
+        setTimeout(() => setShowLoader(false), 1000);
+    }, [user]);
+
     return (
         <>
+            {showLoader ? <LoaderFullscreen /> : null}
             <Banner />
             <div className="space-y-32">
                 <Stats />
