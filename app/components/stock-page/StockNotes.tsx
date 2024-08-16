1bf98364b22aba1867578a0ec38ff5cce74f23ce
Sean <seanreilly123@hotmail.com>
2024-08-16 13:48:18 +1000

    Landing banner just one button now, loading to AI notes, AI notes first still showing bug fixed

--- app/components/stock-page/StockNotes.tsx
+++ app/components/stock-page/StockNotes.tsx
@@ -62,78 +62,61 @@ const StockNotes = ({ ticker, name, type }: Props) => {
     return (
         <div className="flex-1">
             <h2 className="text-2xl font-semibold mb-2">My notes</h2>
-            {user ? (
-                <div>
+            <div>
+                <ul className="w-full space-y-3 mb-4">
                     {isLoading ? (
-                        <div className="space-y-3 mb-4">
-                            <Skeleton active paragraph={{ rows: 1 }} />
-                            <Skeleton active paragraph={{ rows: 1 }} />
-                            <Skeleton active paragraph={{ rows: 1 }} />
-                        </div>
+                        <Skeleton active />
                     ) : savedStock?.notes?.length > 0 ? (
-                        <ul className="w-full space-y-3 mb-4">
-                            {savedStock?.notes?.map((note: TNote) => (
-                                <li key={note.id}>
-                                    <div className="flex items-center space-x-4 rtl:space-x-reverse">
-                                        <div className="flex-1 min-w-0">
-                                            <p className="text-base font-medium text-gray-900">
-                                                {note.text}
-                                            </p>
-                                        </div>
-                                        <EditNotesButton
-                                            note={note}
-                                            ticker={ticker}
-                                        />
+                        savedStock?.notes?.map((note: TNote) => (
+                            <li key={note.id}>
+                                <div className="flex items-center space-x-4 rtl:space-x-reverse">
+                                    <div className="flex-1 min-w-0">
+                                        <p className="text-base font-medium text-gray-900">
+                                            {note.text}
+                                        </p>
                                     </div>
-                                </li>
-                            ))}
-                            <AINotesList
-                                ticker={ticker}
-                                name={name}
-                                type={type}
-                            />
-                        </ul>
+                                    <EditNotesButton
+                                        note={note}
+                                        ticker={ticker}
+                                    />
+                                </div>
+                            </li>
+                        ))
                     ) : (
-                        <>
+                        <div className="pb-1">
                             <EmptyState page="Notes" />
-                            <ul className="space-y-3 mb-4 mt-4">
-                                <AINotesList
-                                    ticker={ticker}
-                                    name={name}
-                                    type={type}
-                                />
-                            </ul>
-                        </>
-                    )}
-                    <form onSubmit={handleNewNote}>
-                        <div className="w-full mb-4 rounded-lg border bg-gray-700 border-gray-600">
-                            <div className="px-4 py-2 rounded-t-lg bg-gray-800">
-                                <label className="sr-only">Your note</label>
-                                <textarea
-                                    rows={4}
-                                    className="w-full px-0 text-base text-white border-0 bg-gray-800  placeholder-gray-400 focus:outline-none"
-                                    value={noteText}
-                                    onChange={(e) =>
-                                        setNoteText(e.currentTarget.value)
-                                    }
-                                    placeholder="Write a note..."
-                                    required
-                                    maxLength={NOTE_MAX_LENGTH}
-                                ></textarea>
-                            </div>
-                            <div className="flex items-center justify-between px-3 py-2 border-t border-gray-600">
-                                <Button type="submit">Add note</Button>
-                                <span className="text-xs text-white">
-                                    {noteText.length} / {NOTE_MAX_LENGTH}{" "}
-                                    characters
-                                </span>
-                            </div>
                         </div>
-                    </form>
-                </div>
-            ) : (
-                <p>You must be logged in to make notes about a stock.</p>
-            )}
+                    )}
+                    {isLoading ? null : (
+                        <AINotesList ticker={ticker} name={name} type={type} />
+                    )}
+                </ul>
+
+                <form onSubmit={handleNewNote}>
+                    <div className="w-full mb-4 rounded-lg border bg-gray-700 border-gray-600">
+                        <div className="px-4 py-2 rounded-t-lg bg-gray-800">
+                            <label className="sr-only">Your note</label>
+                            <textarea
+                                rows={4}
+                                className="w-full px-0 text-base text-white border-0 bg-gray-800  placeholder-gray-400 focus:outline-none"
+                                value={noteText}
+                                onChange={(e) =>
+                                    setNoteText(e.currentTarget.value)
+                                }
+                                placeholder="Write a note..."
+                                required
+                                maxLength={NOTE_MAX_LENGTH}
+                            ></textarea>
+                        </div>
+                        <div className="flex items-center justify-between px-3 py-2 border-t border-gray-600">
+                            <Button type="submit">Add note</Button>
+                            <span className="text-xs text-white">
+                                {noteText.length} / {NOTE_MAX_LENGTH} characters
+                            </span>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
