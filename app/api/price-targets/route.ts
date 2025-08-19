5fb12070c19b179d4d229e04c79bc0663f9f6287
Sean <seanreilly123@hotmail.com>
2025-08-19 16:27:17 +1000

    Emails sent on price target hit

--- app/api/price-targets/route.ts
+++ app/api/price-targets/route.ts
@@ -1,3 +1,4 @@
+import nodemailer from "nodemailer";
 import { getUsers } from "@/server/actions/db";
 
 export async function GET(req: Request) {
@@ -9,11 +10,9 @@ export async function GET(req: Request) {
                     user.stocks.map((stock: { ticker: string }) => stock.ticker)
                 )
             ),
-        ];
+        ].slice(0, 4);
 
-        const smallTickers = tickers;
-
-        const reqs = smallTickers.map(async (ticker) => {
+        const reqs = tickers.map(async (ticker) => {
             const res = await fetch(
                 `https://api.polygon.io/v2/aggs/ticker/${ticker}/prev?adjusted=true&apiKey=${process.env.NEXT_PUBLIC_POLYGON_API_KEY}`
             );
@@ -23,23 +22,73 @@ export async function GET(req: Request) {
 
         const responses = await Promise.all(reqs);
 
-        for (let i = 0; i < responses.length; i++) {
-            const response = responses[i];
-            const hasTicker: any[] = [];
-            users.forEach((user) => {
-                const stock = user.stocks.find(
-                    (stock: { ticker: string; targetPrice: number | null }) =>
-                        stock.ticker == response.T && stock.targetPrice
-                );
-                if (stock && response.c > stock.targetPrice) {
-                    hasTicker.push([response.T, user.email, stock.targetPrice]);
-                }
-            });
-            console.log(hasTicker);
-        }
+        await sendEmails(responses, users);
 
-        return Response.json(responses);
+        return Response.json(true);
     } catch (error) {
         console.error(error);
     }
 }
+
+const sendEmails = async (responses: any[], users: any[]) => {
+    const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: "seanreilly52@gmail.com",
+            pass: "xgywkihemptapjyj",
+        },
+    });
+
+    for (let i = 0; i < responses.length; i++) {
+        const response = responses[i];
+        const hasTicker: any[] = [];
+        for (let j = 0; j < users.length; j++) {
+            const user = users[j];
+            const stock = user.stocks.find(
+                (stock: { ticker: string; targetPrice: number | null }) =>
+                    stock.ticker == response.T && stock.targetPrice
+            );
+            if (
+                stock &&
+                ((response.c > stock.targetPrice && stock.holding) ||
+                    (response.c < stock.targetPrice && !stock.holding))
+            ) {
+                hasTicker.push([response.T, user.email, stock.targetPrice]);
+                await transporter.sendMail({
+                    from: "Sean",
+                    to: "seanreilly123@hotmail.com",
+                    subject: `${response.T} has hit your price target!`,
+                    text: "Move now to handle your price target.",
+                });
+            }
+        }
+        console.log(hasTicker);
+    }
+
+    // const { name, email, message } = req.body;
+
+    // const transporter = nodemailer.createTransport({
+    //     host: "smtp.gmail.com",
+    //     port: 587,
+    //     auth: {
+    //         user: process.env.GMAIL_USERNAME,
+    //         pass: process.env.GMAIL_PASSWORD,
+    //     },
+    // });
+
+    // var mailOptions = {
+    //     from: "Aotearoa DJ Academy",
+    //     to: "seanreilly123@hotmail.com", // TODO: add result email
+    //     subject: "Welcome to Aotearoa DJ Academy",
+    //     html: defineEmailHTML(req.result),
+    // };
+
+    // transporter.sendMail(mailOptions, function (error, info) {
+    //     if (error) {
+    //         console.log(error);
+    //         res.send(500, err.message);
+    //     } else {
+    //         res.status(200).json(true);
+    //     }
+    // });
+};
--- hooks/useHandlePriceTargetChecks.ts
+++ hooks/useHandlePriceTargetChecks.ts
@@ -1,7 +1,5 @@
-import React from "react";
 import { useMutation } from "@tanstack/react-query";
 import { checkPriceTargets } from "@/server/actions/stocks";
-import { TStockPrice } from "@/utils/types";
 
 const useHandlePriceTargetChecks = () => {
     return useMutation({
