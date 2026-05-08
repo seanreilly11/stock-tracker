862e5d00f67c0c838fdd90c2a358c87d1f730ec1
Sean Reilly <seanreilly123@hotmail.com>
2026-05-08 12:09:39 +0200

    chore: remove unused hooks for fetching AI notes, suggestions, stock details, news, prices, and search functionality

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,122 +1,119 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useAuth } from "@/lib/hooks/useAuth";
 import { useForm } from "react-hook-form";
 import Button from "../components/ui/Button";
 import { addFeedback } from "@/lib/api/db";
-import usePopup from "@/hooks/usePopup";
+import usePopup from "@/lib/hooks/usePopup";
 
 type FormData = {
-    name: string;
-    email: string;
-    message: string;
+  name: string;
+  email: string;
+  message: string;
 };
 
 const Page = () => {
-    const { user } = useAuth();
-    const { contextHolder, messagePopup } = usePopup();
-    const [loading, setLoading] = useState(false);
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors },
-    } = useForm<FormData>({
-        defaultValues: { email: user?.email || "", message: "", name: "" },
-    });
-    const onSubmit = handleSubmit(async ({ name, email, message }) => {
-        setLoading(true);
-        await addFeedback(email, message, name, user?.id);
-        setLoading(false);
-        reset();
-        messagePopup("success", "Thanks! Your message has been sent.");
-    });
+  const { user } = useAuth();
+  const { contextHolder, messagePopup } = usePopup();
+  const [loading, setLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({
+    defaultValues: { email: user?.email || "", message: "", name: "" },
+  });
+  const onSubmit = handleSubmit(async ({ name, email, message }) => {
+    setLoading(true);
+    await addFeedback(email, message, name, user?.id);
+    setLoading(false);
+    reset();
+    messagePopup("success", "Thanks! Your message has been sent.");
+  });
 
-    return (
-        <>
-            {contextHolder}
-            <div className="w-full max-w-md mx-auto">
-                <form
-                    className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-                    onSubmit={onSubmit}
-                >
-                    <div className="mb-4">
-                        <h2 className="text-2xl font-semibold mb-1">
-                            Contact us!
-                        </h2>
-                        <p className="text-sm">
-                            We'd love to hear any feedback, questions or ideas
-                            you have on how we could improve Bullrush for
-                            everyone.
-                        </p>
-                    </div>
-                    <div className="mb-3">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            Name
-                        </label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                            type="text"
-                            placeholder="Name"
-                            {...register("name")}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            Email *
-                        </label>
-                        <input
-                            className={
-                                "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" +
-                                (errors.email ? " border-red-500" : "")
-                            }
-                            type="email"
-                            placeholder="Email"
-                            {...register("email", {
-                                required: {
-                                    value: true,
-                                    message: "Please enter your email",
-                                },
-                            })}
-                        />
-                        {errors.email ? (
-                            <p className="text-red-500 text-xs italic">
-                                {errors.email.message}
-                            </p>
-                        ) : null}
-                    </div>
-                    <div className="mb-3">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            Message *
-                        </label>
-                        <textarea
-                            className={
-                                "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" +
-                                (errors.message ? " border-red-500" : "")
-                            }
-                            rows={4}
-                            placeholder="Your message"
-                            {...register("message", {
-                                required: {
-                                    value: true,
-                                    message: "Please enter your message",
-                                },
-                            })}
-                        />
-                        {errors.message ? (
-                            <p className="text-red-500 text-xs italic">
-                                {errors.message.message}
-                            </p>
-                        ) : null}
-                    </div>
-                    <Button loading={loading} type="submit" className="w-full">
-                        Send message
-                    </Button>
-                </form>
-            </div>
-        </>
-    );
+  return (
+    <>
+      {contextHolder}
+      <div className="w-full max-w-md mx-auto">
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={onSubmit}
+        >
+          <div className="mb-4">
+            <h2 className="text-2xl font-semibold mb-1">Contact us!</h2>
+            <p className="text-sm">
+              We'd love to hear any feedback, questions or ideas you have on how
+              we could improve Bullrush for everyone.
+            </p>
+          </div>
+          <div className="mb-3">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Name
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="Name"
+              {...register("name")}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Email *
+            </label>
+            <input
+              className={
+                "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" +
+                (errors.email ? " border-red-500" : "")
+              }
+              type="email"
+              placeholder="Email"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Please enter your email",
+                },
+              })}
+            />
+            {errors.email ? (
+              <p className="text-red-500 text-xs italic">
+                {errors.email.message}
+              </p>
+            ) : null}
+          </div>
+          <div className="mb-3">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Message *
+            </label>
+            <textarea
+              className={
+                "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" +
+                (errors.message ? " border-red-500" : "")
+              }
+              rows={4}
+              placeholder="Your message"
+              {...register("message", {
+                required: {
+                  value: true,
+                  message: "Please enter your message",
+                },
+              })}
+            />
+            {errors.message ? (
+              <p className="text-red-500 text-xs italic">
+                {errors.message.message}
+              </p>
+            ) : null}
+          </div>
+          <Button loading={loading} type="submit" className="w-full">
+            Send message
+          </Button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Page;
